ea6e68ecf8ca886df03287d2b498b77c293baa73
vladmsnk <moiseenkov-v@mail.ru>
2023-03-08 21:57:48 +0300

    changed user table constraints

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS users(
-    id uuid PRIMARY KEY ,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
+    id uuid,
+    username VARCHAR(255),
+    email VARCHAR(255),
     password VARCHAR(2048),
-    number VARCHAR(255) UNIQUE NOT NULL
+    number VARCHAR(255),
+    CONSTRAINT users_pk  PRIMARY KEY (id, username, email) -- делаю pk на 3 поля, чтобы был индекс по username и email
 );
