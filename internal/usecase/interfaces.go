2f2f8b567286573a03e8c42266571d15427dd6a8
vladmsnk <moiseenkov-v@mail.ru>
2023-03-08 21:33:10 +0300

    .

--- internal/usecase/interfaces.go
+++ internal/usecase/interfaces.go
@@ -2,9 +2,7 @@ package usecase
 
 import (
 	"auth/vladmsnk/internal/dto"
-	"auth/vladmsnk/internal/entity"
 	"context"
-	"github.com/google/uuid"
 )
 
 type (
@@ -15,11 +13,5 @@ type (
 	Auth interface {
 		CreateUser(ctx context.Context, request dto.UserRegisterRequest) (dto.UserRegisterResponse, error)
 		GenerateToken(ctx context.Context, request dto.UserLoginRequest) (dto.UserLoginResponse, error)
-	}
-
-	AuthRepo interface {
-		SaveUser(ctx context.Context, user *entity.User) (uuid.UUID, error)
-		FindUserUserByEmail(ctx context.Context, email string) (*entity.User, error)
-		FindUserUserByUsernameAndPassword(ctx context.Context, username, password string) (*entity.User, error)
 	}
 )
